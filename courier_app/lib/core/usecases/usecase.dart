949399b08170860ec3e15690305fb105cad332d0
Victor Oseghale <voseghale1@gmail.com>
2025-09-28 02:26:29 +0200

    Changed the use of Type to ObjectType to avoid conflick with global type

--- courier_app/lib/core/usecases/usecase.dart
+++ courier_app/lib/core/usecases/usecase.dart
@@ -3,9 +3,9 @@ import 'package:equatable/equatable.dart';
 import 'package:delivery_app/core/error/failures.dart';
 
 /// Base class for all use cases
-abstract class UseCase<Type, Params> {
+abstract class UseCase<ObjectType, Params> {
   /// Executes the use case
-  Future<Either<Failure, Type>> call(Params params);
+  Future<Either<Failure, ObjectType>> call(Params params);
 }
 
 /// Parameters class for use cases that don't require parameters
